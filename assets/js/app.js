a41f84620f768fd96e94e3d27d3624c5e0604490
Jorge Lucas <jlucaspaixao@outlook.com>
2020-07-16 18:49:47 -0300

    Desenvolvimento do Header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,36 @@
+import anime from 'animejs'
 import './jquery'
+
+anime({
+  // left to right
+  targets: '.shape-one, .shape-six',
+  translateX: 100,
+  duration: 10000,
+})
+
+anime({
+  // bottom to top
+  targets: '.shape-two',
+  translateY: -100,
+  duration: 10000,
+})
+
+anime({
+  // top to bottom
+  targets: '.shape-tree',
+  translateY: 100,
+  duration: 10000,
+})
+
+anime({
+  // right to left
+  targets: '.shape-four, .shape-seven',
+  translateX: -100,
+  duration: 10000,
+})
+
+anime({
+  targets: '.shape-five',
+  scale: [0, 1],
+  duration: 10000,
+})
